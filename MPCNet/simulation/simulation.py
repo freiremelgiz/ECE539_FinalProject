f3cbbd0d6df7377847c7722e791922fc4c4e3817
Victor Freire <freiremelgiz@wisc.edu>
2022-04-11 13:10:34 -0500

    Working example

--- MPCNet/simulation/simulation.py
+++ MPCNet/simulation/simulation.py
@@ -4,41 +4,47 @@ from scipy.integrate import RK45
 from typing import Callable
 from datetime import datetime
 
+
 class Simulation:
 
-    # The initial state (x, y, v, psi)
-    initial: np.ndarray
-
-    # The controller (time, state) -> control (e.g. v dot, psi dot)
-    #controller: Callable[[float, np.ndarray], np.ndarray]
-    controller: Callable[[float, np.ndarray], np.ndarray]
-
-    # The rate (Hz) of the controller
-    controlRate: float
-
-    # Time step
-    timeStep: float
-
-    # Simulated states
-    states: np.ndarray
-    # Simulated time steps
-    times: np.ndarray
+#    # The initial state (x, y, v, psi)
+#    initialState: np.ndarray
+#
+#    # The controller (time, state) -> control (e.g. v dot, psi dot)
+#    #controller: Callable[[float, np.ndarray], np.ndarray]
+#    controller: Callable[[float, np.ndarray], np.ndarray]
+#
+#    # The rate (Hz) of the controller
+#    controlRate: float
+#
+#    # Time step
+#    timeStep: float
+#
+#    # Simulated states
+#    states: np.ndarray
+#    # Simulated time steps
+#    times: np.ndarray
 
     def __init__(
             self,
             initialState: np.ndarray,
+            finalState:np.ndarray,
             controlRate: float,
             controller: Callable[[float, np.ndarray], np.ndarray]):
         """
         Initialize the simulator
 
         :param initialState np.ndarray: The initial state
+        :param finalState np.ndarray: The target final state
         :param controlRate float: The control rate (Hz)
         :param controller Callable[[float, np.ndarray], np.ndarray]: The controlelr
             callback
         """
-        # Initial state zeroed out
+        # Initial state
         self.initialState = initialState
+        # Final state
+        self.finalState = finalState
+        self.reached_goal = self._is_near_goal(initialState[0:2])
 
         # Save the controller
         self.controller = controller
@@ -51,22 +57,20 @@ class Simulation:
     def runSimulation(
             self,
             duration: float,
-            goal: np.ndarray,
             quiet=False):
         """
         Run the simulation
 
         :param duration float: Duration to run it for.
-        :param goal np.ndarray: The final (goal) state
+        :param quiet boolean: Display iterations
         """
         times = [0.0]
         states = [self.initialState]
-        reached_goal = False
 
         startTime = datetime.now()
         iterCount = 0
 
-        while(times[-1] < duration and not reached_goal):
+        while(times[-1] < duration and not self.reached_goal):
             if(not quiet):
                 print(f"{iterCount}: Sim time: {times[-1]}/{duration}     Wall Time: {datetime.now()-startTime}")
 
@@ -79,12 +83,16 @@ class Simulation:
 
             iterCount += 1
 
-            # Check goal
-            if np.linalg.norm(goal[0:2]-states[-1][0:2]) <= 5:
-                reached_goal = True
+            # Check goal reached
+            if self._is_near_goal(states[-1][0:2]):
+                self.reached_goal = True
 
         self.times = np.array(times)
         self.states = np.array(states)
+
+    # Is the passed point r = (x,y) near the goal state?
+    def _is_near_goal(self, r):
+        return np.linalg.norm(self.finalState[0:2] - r) <= 5
 
     def dynamics(self, time, state) -> np.ndarray:
         x = state[0]
--- sim_example.py
+++ sim_example.py
@@ -8,18 +8,12 @@ from MPCNet.controller.MPC import MPC
 
 
 ### SIM EXAMPLE
-#goal = np.array([100.0, 5.0, 0.0, 0.0])
-#controller = partial(pyomo_controller.mpcController,
-#                     finalState=goal,
-#                     plot=True)
-#
-#sim = simulation.Simulation(
-#        np.array([0.0, 0.0, 10.0, 0.0]),
-#        100.0,
-#        controller)
-#
-#sim.runSimulation(20.0, goal)
-#simulation.plotSimulation(sim, f"run.png")
+initial = np.array([0.0, 0.0, 10.0, 0.0])
+final = np.array([100.0, 5.0, 0.0, 0.0])
+controller = MPC(final, plot=True)
+sim = simulation.Simulation(initial, final, 10.0, controller)
+sim.runSimulation(20.0)
+simulation.plotSimulation(sim, f"run.png")
 
 
 ### PYOMO EXAMPLE
@@ -29,7 +23,7 @@ from MPCNet.controller.MPC import MPC
 
 
 ### PYOMO CLASS EXAMPLE
-initial = [70.0, 3.0, 10.0, 0.0]
-final = [100.0, 5.0, 0.0, 0.0]
-controller = MPC(final, plot=True)
-controller(initial)
+#initial = [70.0, 3.0, 10.0, 0.0]
+#final = [100.0, 5.0, 0.0, 0.0]
+#controller = MPC(final, plot=True)
+#controller(initial)

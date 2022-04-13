122849080159f36921141e531f8e579011113dfb
Victor Freire <freiremelgiz@wisc.edu>
2022-04-13 10:05:47 -0500

    10Hz controller

--- sim_example.py
+++ sim_example.py
@@ -4,13 +4,24 @@ from MPCNet.simulation import simulation
 import numpy as np
 from MPCNet.controller import pyomo_controller
 from functools import partial
-from MPCNet.controller.MPC import MPC
+from MPCNet.controller.MPC import MPC, MPCParams
 
 
-### SIM EXAMPLE
+### SIM EXAMPLE (100 Hz)
+#initial = np.array([0.0, 0.0, 0.0, 0.0])
+#final = np.array([20.0, 20.0, 0.0, np.pi/2])
+#controller = MPC(final, plot=True)
+#sim = simulation.Simulation(initial, final, 50.0, controller)
+#sim.runSimulation(20.0)
+#simulation.plot_path(sim, f"path_sim.png")
+#simulation.plot_input(sim, f"input_sim.png")
+
+### SIM EXAMPLE (10 Hz)
 initial = np.array([0.0, 0.0, 0.0, 0.0])
 final = np.array([20.0, 20.0, 0.0, np.pi/2])
-controller = MPC(final, plot=True)
+params = MPCParams()
+params.dt = 0.1
+controller = MPC(final, params=params, plot=True)
 sim = simulation.Simulation(initial, final, 50.0, controller)
 sim.runSimulation(20.0)
 simulation.plot_path(sim, f"path_sim.png")
